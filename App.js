ecfdf459d4838121f0e7315bc3db6ba3b4a392bb
Jonas Hackenberg <jonas@arkulpa.at>
2020-03-15 16:28:39 +0100

    Display logo
    https://docs.expo.io/versions/v36.0.0/tutorial/image/#displaying-the-image-in-the-app

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={{ width: 305, height: 159 }} />
+
       <Text style={{color: '#888', fontSize: 18}}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>

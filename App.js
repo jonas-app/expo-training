9edabf21fc7e052719bbab8f9467f3baebcfc3ab
Jonas Hackenberg <jonas@arkulpa.at>
2020-03-15 16:22:00 +0100

    Style info text inline
    https://docs.expo.io/versions/v36.0.0/tutorial/text/#adding-style

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Text style={{color: '#888', fontSize: 18}}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
     </View>
   );
 }
